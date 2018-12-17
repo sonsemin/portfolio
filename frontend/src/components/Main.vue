cde4468c933510086e0fdda0f48869dd81fc39b7
sonsemin <sonsemin@daum.net>
2018-12-17 22:35:11 +0900

    Revert "공부중~~"
    This reverts commit e0e09213c6343b959e0edb3bbdb3e24063b0a059.

--- frontend/src/components/Main.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div id="app">
-    이거로와라!!!!!
-    <router-link to="/test">test고고고</router-link>
-    <router-link to="/vuextest">vuex테스트</router-link>
-  </div>
-</template>
--- src/main/java/com/portfolio/board/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.portfolio.board.repository;
-
-import com.portfolio.board.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
