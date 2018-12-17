e0e09213c6343b959e0edb3bbdb3e24063b0a059
sonsemin <sonsemin@daum.net>
2018-12-17 22:33:26 +0900

    공부중~~
    ㄱㄱㄱㄱ

---
+++ src/test/java/com/portfolio/board/BoardRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.portfolio.board;
+
+import com.portfolio.board.entity.Board;
+import com.portfolio.board.repository.BoardRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoardRepositoryTest {
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    @Test
+    public void test(){
+        boardRepository.save(Board.builder()
+        .title("제목입니다")
+        .user_id("sonsemin")
+        .build());
+    }
+}
